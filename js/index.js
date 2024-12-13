851308d1413f9be0a4c743f89e372b54b1c0852b
CHAMPES <nathan.champes@gmail.com>
2024-12-13 09:14:04 +0100

    Réparation erreuré

--- js/index.js
+++ js/index.js
@@ -18,13 +18,14 @@ function threeD() {
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
+    function animate(){
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        
+        render.render(scene, camera);
+    }
+
     animate();
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    render.render(scene, camera);
-}
